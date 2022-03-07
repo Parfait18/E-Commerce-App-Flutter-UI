cad76665110fc82c4034e7ee5d7b51b0149147f7
parfait18 <ahouantoparfaitsylvanus51@gmail.com>
2022-03-07 20:10:13 +0100

    feat:solving splash screen

--- lib/sreens/splash/components/body.dart
+++ lib/sreens/splash/components/body.dart
@@ -11,6 +11,20 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  List<Map<String,String>> splashData= [
+    {
+      "text": "Welcome in APS Shop, Lets's shop",
+      "image": "assets/images/splash_1.png"
+    },
+    {
+      "text": "We help people connect with store \n around United State of America",
+      "image": "assets/images/splash_2.png"
+    },
+    {
+      "text": "We show the easy way to shop. Just stay at home with us",
+      "image": "assets/images/splash_3.png"
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -20,7 +34,11 @@ class _BodyState extends State<Body> {
           children: <Widget>[
             Expanded(
               flex: 3,
-              child: SplashContent()
+              child: PageView.builder(
+                  itemCount: splashData.length,
+                  itemBuilder: (context,index)=> SplashContent(
+                  text: "Welcome in APS Shop, Lets's shop",
+                  image: "assets/images/splash_1.png"))
             ),
             Expanded(
                 flex:2,
@@ -32,8 +50,10 @@ class _BodyState extends State<Body> {
   }
 }
  class SplashContent extends StatelessWidget {
-   const SplashContent({Key? key}) : super(key: key);
- 
+   const SplashContent({Key? key, required this.text, required this.image}) : super(key: key);
+   final String text,image;
+
+
    @override
    Widget build(BuildContext context) {
      return SafeArea(
@@ -52,10 +72,10 @@ class _BodyState extends State<Body> {
                              color: cPrimaryColor,
                              fontWeight: FontWeight.bold
                          ),),
-                       Text("Welcome in my shop APP,Let's Shop !!"),
+                       Text(text),
                        Spacer(flex: 2),
                        Image.asset(
-                         "assets/images/splash_1.png",
+                       image,
                          height: getProportionateScreenHeight(265),
                          width: getProportionateScreenWidth(235),
                        )
