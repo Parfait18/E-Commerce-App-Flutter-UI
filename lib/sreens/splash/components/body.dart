820968bba3dd4f7ab4cb8d3ad86bb1a789dea18a
parfait18 <ahouantoparfaitsylvanus51@gmail.com>
2022-03-08 20:06:51 +0100

    feat:solving splash screen

--- lib/sreens/splash/components/body.dart
+++ lib/sreens/splash/components/body.dart
@@ -33,12 +33,25 @@ class _BodyState extends State<Body> {
         child: Column(
           children: <Widget>[
             Expanded(
-              flex: 3,
-              child: PageView.builder(
-                  itemCount: splashData.length,
-                  itemBuilder: (context,index)=> SplashContent(
-                  text: "Welcome in APS Shop, Lets's shop",
-                  image: "assets/images/splash_1.png"))
+                flex: 3,
+                child: Column(
+                    children: <Widget>[
+                      Spacer(),
+                      Text("APS SHOP",
+                        style: TextStyle(
+                            fontSize: getProportionateScreenWidth(36),
+                            color: cPrimaryColor,
+                            fontWeight: FontWeight.bold
+                        ),),
+                      Text("Welcome in APS Shop, Lets's shop"),
+                      Spacer(flex: 2),
+                      Image.asset(
+                        "assets/images/splash_1.png",
+                        height: getProportionateScreenHeight(265),
+                        width: getProportionateScreenWidth(235),
+                      )
+                    ]
+                )
             ),
             Expanded(
                 flex:2,
@@ -56,39 +69,7 @@ class _BodyState extends State<Body> {
 
    @override
    Widget build(BuildContext context) {
-     return SafeArea(
-       child: SizedBox(
-         width: double.infinity,
-         child: Column(
-           children: <Widget>[
-             Expanded(
-                 flex: 3,
-                 child: Column(
-                     children: <Widget>[
-                       Spacer(),
-                       Text("APS SHOP",
-                         style: TextStyle(
-                             fontSize: getProportionateScreenWidth(36),
-                             color: cPrimaryColor,
-                             fontWeight: FontWeight.bold
-                         ),),
-                       Text(text),
-                       Spacer(flex: 2),
-                       Image.asset(
-                       image,
-                         height: getProportionateScreenHeight(265),
-                         width: getProportionateScreenWidth(235),
-                       )
-                     ]
-                 )
-             ),
-             Expanded(
-                 flex:2,
-                 child: SizedBox())
-           ],
-         ),
-       ),
-     );
+     return Column();
    }
  }
 
